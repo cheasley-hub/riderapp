1e4ee275d3ac4ab2082c4f3ab85c11bd8d03b892
Cody Heasley <heasley100@gmail.com>
2019-12-06 16:01:40 -0500

    Init

---
+++ Activities/SplashActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace RidesApp.Activities
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/myTheme.splash", MainLauncher = true, NoHistory = true, ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(LoginActivity));
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,72 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Firebase;
+using Firebase.Database;
+
+namespace RidesApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = false)]
+
+    public class MainActivity : AppCompatActivity
+    {
+        Button testConnection;
+
+        FirebaseDatabase database;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            testConnection = (Button)FindViewById(Resource.Id.myButton);
+
+            testConnection.Click += TestConnection_Click;
+
+        }
+
+        private void TestConnection_Click(object sender, System.EventArgs e)
+        {
+            initializeDatabase();
+        }
+
+        void initializeDatabase()
+        {
+            var app = FirebaseApp.InitializeApp(this);
+
+            if (app == null)
+            {
+                var options = new FirebaseOptions.Builder()
+                    .SetApplicationId("rideappproject")
+                    .SetApiKey("AIzaSyCflazAnYZKhp5g_leYvPSs1SRf0BJLsZc")
+                    .SetDatabaseUrl("https://rideappproject.firebaseio.com/")
+                    .SetStorageBucket("rideappproject.appspot.com")
+                    .Build();
+
+                app = FirebaseApp.InitializeApp(this, options);
+
+                database = FirebaseDatabase.GetInstance(app);
+            }
+            else
+            {
+                database = FirebaseDatabase.GetInstance(app);
+            }
+
+            DatabaseReference dbref = database.GetReference("GetSupport");
+
+            dbref.SetValue("Ticket1");
+
+            Toast.MakeText(this, "Comleted", ToastLength.Short).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
